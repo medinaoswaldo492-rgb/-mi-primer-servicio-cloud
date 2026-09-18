834359d16a619c031564b016c6aa86354d5fea8c
Oswaldo <medinaoswaldo492@gmail.com>
2026-09-18 09:16:35 -0600

    primercloud

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import { useEffect, useState } from "react";
-//hola putita
+
 function App() {
   const [productos, setProductos] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const [categoria, setCategoria] = useState("Todas");
   const [cargando, setCargando] = useState(true);
   const [error, setError] = useState(false);
+
+const productosFiltrados = productos.filter((producto) => {
+  const coincideNombre = producto.nombre
+    .toLowerCase()
+    .includes(busqueda.toLowerCase());
+
+  const coincideCategoria =
+    categoria === "Todas" || producto.categoria === categoria;
+
+  return coincideNombre && coincideCategoria;
+});
 
   const API_URL =
     "https://script.google.com/macros/s/AKfycby3GA0y5BaYdnqXKElqWpnGZhSr2ak8KNuhtQO44DpfQdVgSQd_e0LXHLUCMFklUlBkzQ/exec";
@@ -34,6 +47,34 @@ function App() {
     <div>
       <h1>Mi Primer Servicio Cloud</h1>
 
+      <input
+        type="text"
+        placeholder="Buscar producto..."
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+        style={{
+          padding: "10px",
+          width: "300px",
+          marginBottom: "20px",
+        }}
+      />
+
+      <select
+  value={categoria}
+  onChange={(e) => setCategoria(e.target.value)}
+  style={{
+    padding: "10px",
+    marginBottom: "20px",
+    marginLeft: "10px"
+  }}
+>
+  <option value="Todas">Todas las categorías</option>
+  <option value="Computadoras">Computadoras</option>
+  <option value="Accesorios">Accesorios</option>
+  <option value="Redes">Redes</option>
+  <option value="Almacenamiento">Almacenamiento</option>
+</select>
+
       <p>
         Aplicación React consumiendo una API desarrollada
         con Google Apps Script y Google Sheets
@@ -49,7 +90,7 @@ function App() {
 
       {!cargando &&
         !error &&
-        productos.map((producto) => (
+        productosFiltrados.map((producto) => (
           <div
             key={producto.ID}
             style={{
